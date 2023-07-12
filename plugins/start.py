63aa474e2a18ad66e4184579393b1c0f8bb558f8
Tvshows122 <126122240+Tvshows122@users.noreply.github.com>
2023-07-12 12:32:38 +0530

    start.py

--- plugins/start.py
+++ plugins/start.py
@@ -124,9 +124,7 @@ async def not_joined(client: Client, message: Message):
         [
             InlineKeyboardButton(
                 "Join Channel",
-                url = "https://t.me/+hkOcq4sxcLw5OWU1")
-        ]
-    ]
+                url = client.invitelink)
     try:
         buttons.append(
             [
